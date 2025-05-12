812b0c50806a5b3f0e0d9fd15ae2e9d33fc8c42c
luisa-ganasevici <lganasevici@gmail.com>
2025-05-12 20:55:27 -0300

    Add files via upload

---
+++ Pessoa.java
@@ -0,0 +1,33 @@
+package br.com.fiap.esporte.model;
+
+public class Pessoa {
+
+    private String nome;
+    private String email;
+    private int idade;
+    private Endereco endereco;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
